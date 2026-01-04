596f1227ee88abb75298841aff254d0d57b0abb5
sysakm <sysak-ma@yandex.ru>
2026-01-04 19:54:13 +0200

    Add empty timestamp handling to ingester

--- py/ingest_sqlite.py
+++ py/ingest_sqlite.py
@@ -1,18 +1,28 @@
 # Expects to be executed from the parent folder
-import pandas as pd
+from pathlib import Path
 import sqlite3
 
+import pandas as pd
 
-TXT_PATH = 'data/household_power_consumption.txt'
-DB_PATH = 'data/housepower.sqlite'
+
+ROOT = Path(__file__).resolve().parents[1]
+TXT_PATH = ROOT / "data" / "household_power_consumption.txt"
+DB_PATH  = ROOT / "data" / "housepower.sqlite"
+SCHEMA_PATH = ROOT / "sql" / "00_schema.sql"
 
 
 def main():
-    conn = sqlite3.connect(DB_PATH)
-
     df = pd.read_csv(TXT_PATH, sep=';', na_values='?')
     n_rows_read = len(df)
-    time_index = pd.to_datetime(df['Date'].astype(str) + ' ' + df['Time'].astype(str), format='%d/%m/%Y %H:%M:%S')
+    time_index = pd.to_datetime(
+        df['Date'].astype(str) + ' ' + df['Time'].astype(str),
+        format='%d/%m/%Y %H:%M:%S',
+        errors='coerce'
+    )
+    df = df.loc[time_index.notna()].copy()
+    time_index = time_index.loc[time_index.notna()]
+    n_empty_ts = time_index.isna().sum()
+
     df.drop(['Date', 'Time'], axis=1, inplace=True)
     df.insert(0, 'ts', time_index.dt.strftime('%Y-%m-%d %H:%M:%S'))
     df.rename({
@@ -25,11 +35,11 @@ def main():
         'Sub_metering_3': 'sub_3'
     }, axis=1, inplace=True)
 
-    with open('sql/00_schema.sql', mode='r') as inf:
-        conn.executescript(inf.read())
+    conn = sqlite3.connect(DB_PATH)
+    conn.executescript(SCHEMA_PATH.read_text())
 
     n_rows_inserted = df.to_sql('raw_power', conn, index=False, if_exists='append', method='multi', chunksize=30_000)
-    print(f'Read {n_rows_read}, inserted {n_rows_inserted} rows')
+    print(f'Read {n_rows_read} rows, found {n_empty_ts} empty timestamps, inserted {n_rows_inserted} rows.')
 
     conn.close()
 
