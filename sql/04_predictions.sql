d3aa833413672fe6f6e1b338161fd4262e13c56d
sysakm <sysak-ma@yandex.ru>
2026-01-24 18:50:07 +0200

    Remove NaNs from prediction table, add catboost baseline

--- sql/04_predictions.sql
+++ sql/04_predictions.sql
@@ -3,7 +3,7 @@ CREATE TABLE model_predictions (
     model_name TEXT NOT NULL,
     model_ver INTEGER NOT NULL,
     hour TEXT NOT NULL,
-    pred_value REAL,
+    pred_value REAL NOT NULL,
     UNIQUE(model_name, model_ver, hour)
 );
 CREATE INDEX IF NOT EXISTS idx_model_predictions
